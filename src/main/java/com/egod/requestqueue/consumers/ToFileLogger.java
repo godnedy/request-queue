5b20ffe4913524b09098d4547517f8c2d45b6615
egod <cantcomplain@wp.pl>
2018-07-04 13:47:38 +0200

    Add temporary implementation of writing to file

--- src/main/java/com/egod/requestqueue/consumers/ToFileLogger.java
+++ src/main/java/com/egod/requestqueue/consumers/ToFileLogger.java
@@ -1,12 +1,23 @@
 package com.egod.requestqueue.consumers;
 
 import com.egod.requestqueue.RequestConsumer;
+import lombok.SneakyThrows;
 import org.springframework.http.HttpEntity;
 
-public class ToFileLogger implements RequestConsumer{
+import java.io.File;
+import java.io.PrintWriter;
+
+public class ToFileLogger implements RequestConsumer {
+    public static final String PATH_TO_FILE = "file.txt";
 
     @Override
+    @SneakyThrows
     public void handleEvent(HttpEntity request) {
 
+        File file = new File(PATH_TO_FILE);
+        if (file.exists()) {
+            PrintWriter writer = new PrintWriter(PATH_TO_FILE);
+            writer.println(request.getBody());
+        }
     }
 }
